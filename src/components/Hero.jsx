43324e73ee0f456aaee433b2f472b82df7df5c28
Dylan Meivis <meivisdylan@gmail.com>
2022-10-06 16:37:06 +0200

    Create Documentation page

---
+++ src/components/Hero.jsx
@@ -0,0 +1,171 @@
+import { Fragment } from 'react'
+import Image from 'next/future/image'
+import clsx from 'clsx'
+import Highlight, { defaultProps } from 'prism-react-renderer'
+
+import { Button } from '@/components/Button'
+import blurCyanImage from '@/images/blur-cyan.png'
+import blurIndigoImage from '@/images/blur-indigo.png'
+
+const codeLanguage = 'javascript'
+const code = `export izipack {
+  strategy: 'predictive',
+  engine: {
+    cpus: 12,
+    backups: ['./storage/izipack.wtf'],
+  },
+}`
+
+const tabs = [
+  { name: 'izipack.config.js', isActive: true },
+  { name: 'izipack.json', isActive: false },
+]
+
+function TrafficLightsIcon(props) {
+  return (
+    <svg aria-hidden="true" viewBox="0 0 42 10" fill="none" {...props}>
+      <circle cx="5" cy="5" r="4.5" />
+      <circle cx="21" cy="5" r="4.5" />
+      <circle cx="37" cy="5" r="4.5" />
+    </svg>
+  )
+}
+
+export function Hero() {
+  return (
+    <div className="overflow-hidden bg-slate-900 dark:-mb-32 dark:mt-[-4.5rem] dark:pb-32 dark:pt-[4.5rem] dark:lg:mt-[-4.75rem] dark:lg:pt-[4.75rem]">
+      <div className="py-16 sm:px-2 lg:relative lg:py-20 lg:px-0">
+        <div className="mx-auto grid max-w-2xl grid-cols-1 items-center gap-y-16 gap-x-8 px-4 lg:max-w-8xl lg:grid-cols-2 lg:px-8 xl:gap-x-16 xl:px-12">
+          <div className="relative z-10 md:text-center lg:text-left">
+            <Image
+              className="absolute bottom-full right-full -mr-72 -mb-56 opacity-50"
+              src={blurCyanImage}
+              alt=""
+              width={530}
+              height={530}
+              unoptimized
+              priority
+            />
+            <div className="relative">
+              <p className="inline bg-gradient-to-r from-green-200 via-emerald-400 to-lime-200 bg-clip-text font-display text-5xl tracking-tight text-transparent">
+                Send packages with Izipack.
+              </p>
+              <p className="mt-3 text-2xl tracking-tight text-slate-400">
+                Easily send packages through Europa with our simple API.
+              </p>
+              <div className="mt-8 flex gap-4 md:justify-center lg:justify-start">
+                <Button href="/">Get started</Button>
+                <Button href="https://github.com/izipack" variant="secondary">
+                  View on GitHub
+                </Button>
+              </div>
+            </div>
+          </div>
+          <div className="relative lg:static xl:pl-10">
+            <div className="relative">
+              <Image
+                className="absolute -top-64 -right-64"
+                src={blurCyanImage}
+                alt=""
+                width={530}
+                height={530}
+                unoptimized
+                priority
+              />
+              <Image
+                className="absolute -bottom-40 -right-44"
+                src={blurIndigoImage}
+                alt=""
+                width={567}
+                height={567}
+                unoptimized
+                priority
+              />
+              <div className="absolute inset-0 rounded-2xl bg-gradient-to-tr from-green-300 via-emerald-300/70 to-lime-300 opacity-10 blur-lg" />
+              <div className="absolute inset-0 rounded-2xl bg-gradient-to-tr from-green-300 via-emerald-300/70 to-lime-300 opacity-10" />
+              <div className="relative rounded-2xl bg-[#0A101F]/80 ring-1 ring-white/10 backdrop-blur">
+                <div className="absolute -top-px left-20 right-11 h-px bg-gradient-to-r from-emerald-300/0 via-emerald-300/70 to-emerald-300/0" />
+                <div className="absolute -bottom-px left-11 right-20 h-px bg-gradient-to-r from-green-400/0 via-green-400 to-green-400/0" />
+                <div className="pl-4 pt-4">
+                  <TrafficLightsIcon className="h-2.5 w-auto stroke-slate-500/30" />
+                  <div className="mt-4 flex space-x-2 text-xs">
+                    {tabs.map((tab) => (
+                      <div
+                        key={tab.name}
+                        className={clsx(
+                          'flex h-6 rounded-full',
+                          tab.isActive
+                            ? 'bg-gradient-to-r from-emerald-400/30 via-emerald-400 to-emerald-400/30 p-px font-medium text-emerald-300'
+                            : 'text-slate-500'
+                        )}
+                      >
+                        <div
+                          className={clsx(
+                            'flex items-center rounded-full px-2.5',
+                            tab.isActive && 'bg-slate-800'
+                          )}
+                        >
+                          {tab.name}
+                        </div>
+                      </div>
+                    ))}
+                  </div>
+                  <div className="mt-6 flex items-start px-1 text-sm">
+                    <div
+                      aria-hidden="true"
+                      className="select-none border-r border-slate-300/5 pr-4 font-mono text-slate-600"
+                    >
+                      {Array.from({
+                        length: code.split('\n').length,
+                      }).map((_, index) => (
+                        <Fragment key={index}>
+                          {(index + 1).toString().padStart(2, '0')}
+                          <br />
+                        </Fragment>
+                      ))}
+                    </div>
+                    <Highlight
+                      {...defaultProps}
+                      code={code}
+                      language={codeLanguage}
+                      theme={undefined}
+                    >
+                      {({
+                        className,
+                        style,
+                        tokens,
+                        getLineProps,
+                        getTokenProps,
+                      }) => (
+                        <pre
+                          className={clsx(
+                            className,
+                            'flex overflow-x-auto pb-6'
+                          )}
+                          style={style}
+                        >
+                          <code className="px-4">
+                            {tokens.map((line, lineIndex) => (
+                              <div key={lineIndex} {...getLineProps({ line })}>
+                                {line.map((token, tokenIndex) => (
+                                  <span
+                                    key={tokenIndex}
+                                    {...getTokenProps({ token })}
+                                  />
+                                ))}
+                              </div>
+                            ))}
+                          </code>
+                        </pre>
+                      )}
+                    </Highlight>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
