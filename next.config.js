7d1f668c000b3518ba3932536798cc0bc48042dd
Dylan Meivis <meivisdylan@gmail.com>
2022-10-06 16:53:59 +0200

    Disable image optimization
    Had to disable this for the next export to work

--- next.config.js
+++ next.config.js
@@ -3,6 +3,9 @@ const withMarkdoc = require('@markdoc/next.js')
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    unoptimized: true,
+  },
   pageExtensions: ['js', 'jsx', 'md'],
   swcMinify: true,
   experimental: {
