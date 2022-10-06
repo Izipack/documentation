aa5b376e05662bfeb9651c7dd0c034a903893639
Dylan Meivis <meivisdylan@gmail.com>
2022-10-06 17:12:12 +0200

    Set basePath for Next app
    Set basepath so that hosted static files are found correctly

--- next.config.js
+++ next.config.js
@@ -3,6 +3,7 @@ const withMarkdoc = require('@markdoc/next.js')
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  basePath: '/documentation',
   images: {
     unoptimized: true,
   },
